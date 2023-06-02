ee49e4d7c782e567e9b66db05202386206291cf9
JohnnyVenturas <bogdanbudura@gmail.com>
2023-06-02 20:59:16 +0200

    Adding Base

---
+++ client.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define FYI 0x01
+#define MYM 0x02
+#define END 0x03
+#define TXT 0x04
+#define MOV 0x05
+#define LFT 0x06
+
+void tic_tac_toe();
